6abd4d6adba515722e4b8ac384d6ee02d45629a9
Alexx73 <tristanluna@yahoo.com.ar>
2020-12-03 17:21:32 -0300

    10 ver

--- adiv.js
+++ adiv.js
@@ -1,7 +1,7 @@
-var pistast = ['No soy un león, pero al final somos iguales.. ', 'El roer es mi trabajo...<br> <br>',
+var pistast = [ 'El roer es mi trabajo...<br> <br>',
   'el queso mi aperitivo  <br> <br>', 'y el gato ha sido siempre, mi más temido enemigo ' ];
  
-var clases = ["alert alert-primary", "alert alert-secondary", "alert alert-success", "alert alert-danger",
+var clases = [ "alert alert-secondary", "alert alert-success", "alert alert-danger",
 "alert alert-warning"]
 
 var i = 0;
@@ -10,7 +10,7 @@ var intentos = document.getElementById('intentos');
 var vidas = 4;
 const btn1 = document.getElementById('btn1');
 const quien = document.getElementById('quien');
-
+const raton = ["ratón", "raton"];
 
   // var image = document.getElementById('log1');
   // image.src = "img2/cat 3 w.png";
@@ -19,16 +19,46 @@ const quien = document.getElementById('quien');
 function findejuego() { 
   div.innerHTML = "fin del juego <br>" + 'Has perdido...la respuesta es <br>' + " ratón <br>"
   var image = document.getElementById('log1');
-  // image.className = "animate__animated animate__bounceIn"
+  image.className = "img-fluid max-width: 100% ml-2 animate__animated animate__bounceIn"
   image.src = "img2/mouse-a-1.png" ;
-  btn1.style.visibility = "hidden"
-  quien.style.visibility = "hidden"
+  // btn1.style.visibility = "hidden"
+  quien.style.visibility = "hidden";
+  var boton = document.getElementById('boton');
+  boton.style.visibility = "hidden";
 };
 
+function arriesgar() {
+  event.preventDefault()
+  var arriesgo = document.getElementById("quien").value;
+  console.log(arriesgo);
+  // var n = raton.includes(arriesgo);
 
-const form = document.getElementById('form');
-const log = document.getElementById('log');
-form.addEventListener('click', pista);
+  if (vidas < 1 ) {
+    findejuego()       
+} else {
+  console.log(i);
+  div.className = clases[i] ;
+    div.innerHTML = `pista ${i + 1} ${pistast[i]} <br>`;
+    vidas = vidas - 1;
+  intentos.innerHTML = 'Vidas: ' + vidas + '<br>';
+  i = i + 1;
+    
+  // console.log('vidas ', vidas);
+    // mvidas();
+  setTimeout(() => { console.log(arriesgo); }, 1000);
+  document.getElementById('quien').value = '';
+  return
+  };
+return
+}
+
+
+
+
+// const arriesgar = document.getElementById('arriesgar');
+// arriesgar.addEventListener(submit, arriesgo)
+// const log = document.getElementById('log');
+// form.addEventListener('click', pista);
 
 const form2 = document.getElementById('form2');
 // form2.addEventListener('submit', pista);
@@ -44,18 +74,12 @@ function pista() {
 
     event.preventDefault()
     
-    mvidas();
     if (vidas < 1) {
         findejuego()       
     } else {
       console.log(i);
       div.className = clases[i] ;
       div.innerHTML = `pista ${i + 1} ${pistast[i]} <br>`;
-
-      // document.getElementById("myEle").className = "container-fluid";
-      // 'pista ', i + 1, pistast[i];
-
-
       intentos.innerHTML = 'Vidas: ' + vidas + '<br>';
       i = i + 1;
       vidas = vidas - 1
@@ -67,3 +91,7 @@ function pista() {
 
   return
 }
+
+
+// document.getElementById("myEle").className = "container-fluid";
+      // 'pista ', i + 1, pistast[i];
---
+++ tiempo.js
@@ -0,0 +1,3 @@
+console.log("Hello");
+setTimeout(() => { console.log("World!"); }, 7000);
+
